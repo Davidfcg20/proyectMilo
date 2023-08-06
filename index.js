a2e7e1465419058bd4ca26a6ff8660d8622e5151
Davidfcg20 <david.felipecg20@gmail.com>
2023-08-06 17:11:44 -0500

    CORS problem solution

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
 
 const { /*logErrors, errorHandler*/ boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 8080;
+const whitelist = [`http://localhost:${port}`] //Change for db info
+const corsOptions = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin)){
+      callback(null, true);
+    } else {
+      callback(new Error('acces not allowed'))
+    }
+  }
+}
 
-app.use(express.json())
-
+app.use(express.json());
 routerApi(app);
+app.use(cors(corsOptions));
 
 //app.use(logErrors);
 app.use(boomErrorHandler);
